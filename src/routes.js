fe8fa95d18c99ce41c74fa6cb80064e9ecc07832
AlexTicovschi <alexmticovschi@gmail.com>
2019-04-30 12:11:23 +0100

    project files

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from './HOC/Layout';
+
+const Routes = (props) => {
+  return (
+    <Layout>
+      children 
+    </Layout>
+  )
+}
+
+export default Routes;
